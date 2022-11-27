052e18ecb1d94e1a63007ca9c9fd4567657be1d7
Sch <junhg0211@gmail.com>
2022-11-27 10:59:31 +0900

    자소크어 대문자 변환기 수정

--- converter.js
+++ converter.js
@@ -1,26 +1,35 @@
 function zasok(string) {
     let words = [
-        'g', 'n', 'm', 's', 'j',
-        '\u{E000}=', 'd', 'v', 'z', '\u{E004}=',
-        '\u{E005}=', 'l', '\u{E002}==', 'x', '\u{E009}=',
-        'k', 't', 'p', 'c', 'h',
-        '\u{E000}h', '\u{E001}h', '\u{E002}h', '\u{E003}h', '\u{E004}h',
-        '\u{E000}g', 'r', '\u{E000}m', '\u{E000}s', '\u{E000}j',
-        '\u{E001}n', 'f', '\u{E001}s', '\u{E001}j',
-        '\u{E002}m', '\u{E002}s', '\u{E002}j',
-        '\u{E003}s', '\u{E003}j',
-        '\u{E004}j',
-        'a', 'y', 'o', 'u', 'i', 'w', 'e', '\u{E02E}=',
-        "\u{E028}'", "\u{E029}'", "\u{E02A}'", "\u{E02B}'", "\u{E02C}'", "\u{E02D}'", "\u{E02E}'", "\u{E02F}'", 
-        '\u{E028}"', '\u{E029}"', '\u{E02A}"', '\u{E02B}"', '\u{E02C}"', '\u{E02D}"', '\u{E02E}"', '\u{E02F}"', 
-        '\u{E028}-', '\u{E029}-', '\u{E02A}-', '\u{E02B}-', '\u{E02C}-', '\u{E02D}-', '\u{E02E}-', '\u{E02F}-', 
-        "\u{E030}''", "\u{E031}''", "\u{E032}''", "\u{E033}''", "\u{E034}''", "\u{E035}''", "\u{E036}''", "\u{E037}''", 
+        'G', 'N', 'M', 'S', 'J',
+        '\u{e000}=', 'D', 'V', 'Z', '\u{e004}=',
+        '\u{e005}=', 'L', '\u{e002}==', 'X', '\u{e009}=',
+        'K', 'T', 'P', 'C', 'H',
+        '\u{e000}H', '\u{e001}H', '\u{e002}H', '\u{e003}H', '\u{e004}H',
+        '\u{e000}G', 'R', '\u{e000}M', '\u{e000}S', '\u{e000}J',
+        '\u{e001}N', 'F', '\u{e001}S', '\u{e001}J',
+        '\u{e002}M', '\u{e002}S', '\u{e002}J',
+        '\u{e003}S', '\u{e003}J',
+        '\u{e004}J',
+        'A', 'Y', 'O', 'U', 'I', 'W', 'E', '\u{e02e}=',
+        "\u{e028}'", "\u{e029}'", "\u{e02a}'", "\u{e02b}'", "\u{e02c}'", "\u{e02d}'", "\u{e02e}'", "\u{e02f}'",
+        '\u{e028}"', '\u{e029}"', '\u{e02a}"', '\u{e02b}"', '\u{e02c}"', '\u{e02d}"', '\u{e02e}"', '\u{e02f}"',
+        '\u{e028}-', '\u{e029}-', '\u{e02a}-', '\u{e02b}-', '\u{e02c}-', '\u{e02d}-', '\u{e02e}-', '\u{e02f}-',
+        "\u{e030}''", "\u{e031}''", "\u{e032}''", "\u{e033}''", "\u{e034}''", "\u{e035}''", "\u{e036}''", "\u{e037}''",
     ];
 
     for (let i = 0; i < words.length; i++) {
         if (!(typeof words[i] === 'string')) break;
 
-        words.push([words[i].toUpperCase(), i + 0x50]);
+        let lowercase = "";
+        for (let j = 0; j < words[i].length; j++) {
+            if (0xE000 <= words[i].charCodeAt(j)) {
+                lowercase += String.fromCharCode(words[i].charCodeAt(j) + 0x50);
+            } else {
+                lowercase += words[i][j].toLowerCase();
+            }
+        }
+        words.push([lowercase, i+0x50]);
+
         words[i] = [words[i], i];
     }
 
