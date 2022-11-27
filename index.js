e8d6d887973253f66d747f9b4389ed53dbc21a5f
Sch <junhg0211@gmail.com>
2022-11-27 14:38:10 +0900

    bold, italic 기능 추가

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 let languageSelect;
 let textarea;
+let boldCheck;
+let italicCheck;
 
 function load() {
     languageSelect = document.querySelector("#language");
     textarea = document.querySelector("#textarea");
+    boldCheck = document.querySelector("#bold");
+    italicCheck = document.querySelector("#italic");
+
+    boldCheck.addEventListener('click', e => {
+        if (e.target.checked) {
+            textarea.style.fontWeight = "bold";
+        } else {
+            textarea.style.fontWeight = "normal";
+        }
+    });
+
+    italicCheck.addEventListener('click', e => {
+        if (e.target.checked) {
+            textarea.style.fontStyle = "italic";
+        } else {
+            textarea.style.fontStyle = "";
+        }
+    });
 }
 
 function keyup() {
