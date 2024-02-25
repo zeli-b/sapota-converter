6c53bdd5c9f198376fc04c8a1da9037a186fabb6
Sch <junhg0211@gmail.com>
2024-02-25 20:52:08 +0900

    add keyboard layout preview image

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ let languageSelect;
 let textarea;
 let boldCheck;
 let italicCheck;
+let keyboardLayout;
+
+function updateLayout(to) {
+    keyboardLayout.src = `./res/layouts/${to}.png`;
+}
 
 function load() {
     languageSelect = document.querySelector("#language");
     textarea = document.querySelector("#textarea");
     boldCheck = document.querySelector("#bold");
     italicCheck = document.querySelector("#italic");
+    keyboardLayout = document.querySelector("#keyboard-layout");
+
+    updateLayout(languageSelect.value);
+    languageSelect.addEventListener('change', e => {
+        updateLayout(e.target.value);
+    });
 
     boldCheck.addEventListener('click', e => {
         if (e.target.checked) {
